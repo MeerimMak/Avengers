8b09f147f95f6ebb74c0b543757b97f7469588f4
Turusbekova Begimai <asankadyrova11@gmail.com>
2022-06-21 20:00:53 +0600

    Begimai2

---
+++ src/begimai/Begimai.java
@@ -0,0 +1,7 @@
+package begimai;
+
+public class Begimai {
+    public static void main(String[] args) {
+        System.out.println("Hello my name is Begimai");
+    }
+}
--- src/begimai/Begish.java
+++
@@ -1,7 +0,0 @@
-package begimai;
-
-public class Begish {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
