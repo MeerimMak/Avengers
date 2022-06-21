a4c7e5c1ceecf4f9b47c75f3c2bac173b858f29c
Kasymbaev Temirlan <107786360+Temirlan3@users.noreply.github.com>
2022-06-21 17:51:53 +0600

    New name and age

--- src/Tima/Fmla.java
+++ src/Tima/Fmla.java
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-package Tima;
 
-public class Fmla {
-    public static void main(String[] args) {
-        System.out.println("hello bro");
-=======
 package tima;
 
 public class Fmla {
     public static void main(String[] args) {
-        System.out.println("tretretretre");
->>>>>>> main
-    }
-}
+        System.out.println("hello bro");
+    }}
