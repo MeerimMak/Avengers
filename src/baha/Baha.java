6e4870d8c0218a613f3d2471f6f009b8dc6c4e5f
Bakhtiyar Asankadyrov <asankadyrov.b@mail.ru>
2022-06-21 19:51:51 +0600

    new commit2

--- src/baha/Baha.java
+++ src/baha/Baha.java
@@ -4,5 +4,6 @@ public class Baha {
     public static void main(String[] args) {
         System.out.println("My name is Bakhtiyar");
         System.out.println("new commit");
+        System.out.println("new commit2");
     }
 }
