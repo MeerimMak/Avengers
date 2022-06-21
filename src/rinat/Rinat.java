b1eeacc889bc029d9ab1418f7e989eafcad9479e
Rinat Toktoganov <toktogonovrinat@mail.ru>
2022-06-21 21:05:41 +0600

    qwerty

--- src/rinat/Rinat.java
+++ src/rinat/Rinat.java
@@ -4,5 +4,7 @@ public class Rinat {
     public static void main(String[] args) {
         System.out.println("new class Rinat");
         System.out.println(" www");
+        System.out.println("qwerty");
+
     }
 }
