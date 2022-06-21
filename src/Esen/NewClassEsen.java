6582a1e153fc86d6d705fb6d3d1b2b7f693c589b
Esen Omorbekov <Ezan23@mail.ru>
2022-06-21 19:23:17 +0600

    new mass

--- src/Esen/NewClassEsen.java
+++ src/Esen/NewClassEsen.java
@@ -4,5 +4,6 @@ public class NewClassEsen {
     public static void main(String[] args) {
         System.out.println("My name is Esen");
         System.out.println("New mass");
+        System.out.println("Something");
     }
 }
