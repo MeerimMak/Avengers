604c022d989c4f07656e6c2936abebee14cee5fc
Kasymbaev Temirlan <107786360+Temirlan3@users.noreply.github.com>
2022-06-21 19:40:36 +0600

    new sout Tima

--- src/Tima/NewClassBolot.java
+++ src/Tima/NewClassBolot.java
@@ -4,5 +4,7 @@ public class NewClassBolot {
     public static void main(String[] args) {
         System.out.println("bag");
         System.out.println("hfbksjfnma");
+        System.out.println("as;basikasoirnf");
     }
+
 }
