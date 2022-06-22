4786513b280aa27d7e81e65733563edb2b6134bd
Berdibaeva Kyial <eco.kg-sa@mail.ru>
2022-06-22 17:10:55 +0600

    git reset

--- src/kyial/NewClass.java
+++
@@ -1,9 +0,0 @@
-package kyial;
-
-public class NewClass {
-    public static void main(String[] args) {
-
-        System.out.println("My first branch");
-        System.out.println("Kyial");
-    }
-}
--- src/kyial2/Kyial2.java
+++ src/kyial2/Kyial2.java
@@ -2,6 +2,8 @@ package kyial2;
 
 public class Kyial2 {
     public static void main(String[] args) {
+
         System.out.println("Kyial2");
+        System.out.println("Kyial: \"git reset\"");
     }
 }
